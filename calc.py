75be7dd0e05c0edbf278a457e65c62a232c1cb38
J-Phalen <jphalen335@comcast.net>
2025-05-29 20:22:29 -0400

    Finishing calculator, adding more error checking, and adding additional tests

--- calc.py
+++ calc.py
@@ -26,15 +26,29 @@ def divide(x, y):
 def doathing(action):
     """The main function to figure out which equation we are going to be doing"""
 
-    # TODO Put this if loop inside a while loop.
-    if " " in str(action):
+    if not action:
+        return "Error: You must enter a numeric value."
+    elif " " in str(action):
         return "Error: Your input contained a space."
+    elif "." in str(action):
+        return "Error: Your input contained a decimal point."
+    elif "-" in str(action):
+        return "Error: Your input contained a negative value"
     elif not str(action).isdigit():
         return "Error: You can only enter a numeral as an action."
-    elif not int(action) in range(1, 4):
+    elif not int(action) in range(1, 5):
         return "You can only pick from actions 1/2/3/4."
     else:
-        return "Valid"
+        # Changing this to a dict object for the purpose of testing the results with unittests.
+        return {"Valid": int(action)}
+
+
+def get_inputs(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            logger.error("Error: Please enter a valid number.")
 
 
 # Start execution.
@@ -45,6 +59,38 @@ if __name__ == "__main__":
     logger.info("2. Subtract")
     logger.info("3. Multiply")
     logger.info("4. Divide")
-    action = input("Enter the number of the function you wish to perform.\n")
 
-    doathing(action)
+    # Use a while loop to make sure the action function is a valid operation.
+    while True:
+        action = input("Enter the number of the function you wish to perform.\n")
+        result = doathing(action)
+        if isinstance(result, dict) and "Valid" in result:
+            action = result.get("Valid")
+            break  # break out of the loop when we know the action is valid.
+        else:
+            logger.info(result)
+
+    # Nuking the try/except and turning the inputs into another function, so i can loop
+    # them for valid input just like the action.
+    x = get_inputs("Enter the first number: ")
+    y = get_inputs("Enter the second number: ")
+
+    # Perform the selected operation:
+    # Add
+    if action == 1:
+        logger.info(f"Result: {add(x, y)}")
+    # Subtract
+    elif action == 2:
+        logger.info(f"Result: {subtract(x, y)}")
+    # Multiply
+    elif action == 3:
+        logger.info(f"Result: {multiply(x, y)}")
+    # Divide
+    elif action == 4:
+        logger.info(f"Result: {divide(x, y)}")
+    # Close If/Else statement because well, I can?
+    else:
+        logger.error(
+            "How did you even get to this line? Action should never be a value other than 1-4! "
+            "But this is an else statement to close the if loop."
+        )
--- calc_unittests.py
+++ calc_unittests.py
@@ -1,22 +1,53 @@
 import unittest
 
-from calc import doathing
+import calc
 
 
 class TestDoaThing(unittest.TestCase):
 
+    def test_empty_string(self):
+        self.assertEqual(calc.doathing(""), "Error: You must enter a numeric value.")
+
     def test_input_with_space(self):
-        self.assertEqual(doathing(" 2"), "Error: Your input contained a space.")
+        self.assertEqual(calc.doathing(" 2"), "Error: Your input contained a space.")
 
     def test_non_digit_input(self):
-        self.assertEqual(doathing("Jimmy"), "Error: You can only enter a numeral as an action.")
+        self.assertEqual(calc.doathing("Jimmy"), "Error: You can only enter a numeral as an action.")
 
     def test_number_out_of_range(self):
-        self.assertEqual(doathing("5"), "You can only pick from actions 1/2/3/4.")
-        self.assertEqual(doathing(0), "You can only pick from actions 1/2/3/4.")
+        self.assertEqual(calc.doathing("5"), "You can only pick from actions 1/2/3/4.")
+        self.assertEqual(calc.doathing(0), "You can only pick from actions 1/2/3/4.")
 
     def test_gibberish_input(self):
-        self.assertEqual(doathing("709w283475982w37498qw27"), "Error: You can only enter a numeral as an action.")
+        self.assertEqual(calc.doathing("709w283475982w37498qw27"), "Error: You can only enter a numeral as an action.")
+
+    def test_valid_inputs(self):
+        self.assertEqual(calc.doathing("1"), {"Valid": 1})
+        self.assertEqual(calc.doathing("2"), {"Valid": 2})
+        self.assertEqual(calc.doathing("3"), {"Valid": 3})
+        self.assertEqual(calc.doathing("4"), {"Valid": 4})
+
+    class TestArithmeticOperations(unittest.TestCase):
+
+        def test_add(self):
+            self.assertEqual(calc.add(3, 5), 8)
+            self.assertEqual(calc.add(-1, 1), 0)
+            self.assertEqual(calc.add(0, 0), 0)
+
+        def test_subtract(self):
+            self.assertEqual(calc.subtract(10, 5), 5)
+            self.assertEqual(calc.subtract(-1, -1), 0)
+
+        def test_multiply(self):
+            self.assertEqual(calc.multiply(4, 5), 20)
+            self.assertEqual(calc.multiply(-2, 3), -6)
+
+        def test_divide(self):
+            self.assertEqual(calc.divide(10, 2), 5)
+            self.assertEqual(calc.divide(5, 2), 2.5)
+
+        def test_divide_by_zero(self):
+            self.assertEqual(calc.divide(5, 0), "Error: Division by 0 is impossible dumbass.")
 
 
 if __name__ == "__main__":
